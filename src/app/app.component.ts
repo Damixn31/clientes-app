3233acc7f7518a58079f136687ca5c37b28bd5cb
Damixn31 <damianol29@hotmail.com>
2023-03-09 21:47:05 +0000

    clientes-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Biendenido a Angular';
+
+  curso: string = 'Curso Spring 5 con Angular';
+  profesor: string = 'Andres Guzman';
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,21 @@
+
+import { Cliente } from'./cliente';
+
+export const CLIENTES: Cliente[] = [
+{id: 1, nombre: 'Damian', apellido: 'Olmedo', email: 'dami32@gmail', createAt: '2023-12-11'},
+{id: 2, nombre: 'Nicolas', apellido: 'Olmedo', email: 'nicoOl@gmail', createAt: '2021-07-11'},
+{id: 3, nombre: 'Sandra', apellido: 'Escobar', email: 'San@gmail', createAt: '2023-12-11'},
+{id: 4, nombre: 'Marco', apellido: 'Olmedo', email: 'maco@gmail', createAt: '2021-12-11'},
+{id: 5, nombre: 'Bruno', apellido: 'Lopez', email: 'bru@gmail', createAt: '2015-12-11'},
+{id: 6, nombre: 'Leila', apellido: 'Balcaza', email: 'luli@gmail', createAt: '2023-12-11'},
+{id: 7, nombre: 'Alejandro', apellido: 'Bazquez', email: 'aaaa@gmail', createAt: '2023-12-11'},
+{id: 8, nombre: 'Dario', apellido: 'Gomez', email: 'goDario@gmail', createAt: '2023-12-11'},
+
+{id: 9, nombre: 'Susana', apellido: 'Godino', email: 'susu@gmail', createAt: '2023-12-11'},
+
+{id: 10, nombre: 'Thiago', apellido: 'Balcaza', email: 'pepe@gmail', createAt: '2023-12-11'},
+
+{id: 11, nombre: 'Mia', apellido: 'Perez', email: 'mimi@gmail', createAt: '2023-12-11'}
+
+  ];
+
